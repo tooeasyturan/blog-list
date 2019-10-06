f64e958de4c52b2c8055088ed99ffd34d0106101
Josh Turan <joshturan@Joshs-Air.lan>
2019-10-06 19:34:07 +0200

    updated through 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -9,6 +9,15 @@ blogsRouter.get('/', (request, response) => {
     })
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog.toJSON())
+  } else {
+    response.status(404).end()
+  }
+})
+
 blogsRouter.post('/', async (request, response) => {
 
   const body = request.body
@@ -45,6 +54,27 @@ blogsRouter.post('/', async (request, response) => {
   //   .then(result => {
   //     response.status(201).json(result)
   //   })
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON())
+    })
 })
 
 module.exports = blogsRouter
